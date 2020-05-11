4045898547de81447ad17deae46f0247ae918b25
nikola4work <nikola4work@outlook.com>
2020-05-11 04:44:04 -0400

    prepping app for use, install bootstrap and weather icons

---
+++ weather-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+import Weather from "./component/weather.component"
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <div className="App">
+   <Weather/>
+  </div>;
+}
+
+export default App;
---
+++ weather-app/src/component/weather.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import App from '../App';
+
+
+
+const Weather = () =>{
+
+    return(
+
+    <div className="container">
+      <div className="cards">
+         <h1>London</h1>
+      </div>        
+
+    </div>
+    )
+}
+
+export default Weather;
