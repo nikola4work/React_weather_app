5898f792d8388b3717e9c7bf9b5b8c303aa1177d
nikola4work <nikola4work@outlook.com>
2020-05-11 04:52:03 -0400

    placeholder for data

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 import Weather from "./component/weather.component"
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'weather-icons/css/weather-icons.css'
 
 function App() {
   return <div className="App">
--- weather-app/src/component/weather.component.jsx
+++ weather-app/src/component/weather.component.jsx
@@ -10,10 +10,29 @@ const Weather = () =>{
     <div className="container">
       <div className="cards">
          <h1>London</h1>
-      </div>        
+         <h5 className="py-4">
+             <i className="wi wi-day-sunny display-1"></i>
+         </h5>
+         <h1 className="py-2">25&deg;</h1>
 
+         {/**show max and min temperature */}
+         {minmaxTemp(24,19)}
+
+         <h4 className="py-3">
+             Slow rain
+         </h4>
+      </div>        
     </div>
     )
+}
+
+function minmaxTemp(min,max){
+    return (
+      <h3>
+        <span className="px-4">{min}&deg;</span>
+        <span className="px-4">{min}&deg;</span>
+      </h3>
+    );
 }
 
 export default Weather;
