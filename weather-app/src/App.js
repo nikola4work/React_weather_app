f50cd2b0b62d0ab2e980d8ed9fa0d91cf9b3a1d5
nikola4work <nikola4work@outlook.com>
2020-05-11 05:04:59 -0400

    added open weather key and current weather data api

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -4,11 +4,34 @@ import "./App.css";
 import Weather from "./component/weather.component"
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'weather-icons/css/weather-icons.css'
+import { Component } from "react";
 
-function App() {
-  return <div className="App">
-   <Weather/>
-  </div>;
+// http://api.openweathermap.org/daa/2.5/weather?q=London,uk
+const API_key = "12be9dca0b918a1415beaa5a35fab8fd";
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.satet = {}
+  }
+
+  getWeather = async()=>{
+    const api_call = await fetch(`http://api.openweathermap.org/daa/2.5/weather?q=London,uk&appid=${API_key}`)
+
+{/**CONVERT DATA TO JSON FILE */}
+const response = await api_call.json();
+
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <Weather />
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
